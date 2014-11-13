0118240a46d5a67382520b65eff08d3654f5dba4
RohitK09 <rohitkatyal@outlook.com>
2014-11-12 23:58:19 -0500

    version 1.0

--- Twitter Data Collector/Tweetcollector2/src/code/Driver.java
+++ Twitter Data Collector/Tweetcollector2/src/code/Driver.java
@@ -1,38 +1,51 @@
 package code;
 
+import auth.ConfigBuilder;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import twitter4j.FilterQuery;
-import twitter4j.RateLimitStatusEvent;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
-import auth.ConfigBuilder;
+
+public class Driver
+{
+  private static final transient Logger LOG = LoggerFactory.getLogger(Driver.class);
+  private FilterQuery filter = new FilterQuery();
+  Properties propKeyWord = new Properties();
+  private String[] keywordsArray = null;
+  
+  public static void main(String[] args)
+  {
+    try
+    {
+      InputStream input = new FileInputStream("keywords.properties");
+      
+      Driver dr = new Driver();
+      dr.propKeyWord.load(input);
+      dr.keywordsArray = dr.propKeyWord.getProperty("Keywords").split(",");
+      dr.filter.track(dr.keywordsArray);
+      
+      LOG.info("Initializing...");
+      LOG.debug("Creating Listener...");
+      EnglishStatusListener listener = new EnglishStatusListener();
+      LOG.debug("Creating Stream...");
+      TwitterStream twitterStream = new TwitterStreamFactory(ConfigBuilder.getConfig()).getInstance();
+      
+      twitterStream.addListener(listener);
+      twitterStream.filter(dr.filter);
+      
 
 
-public class Driver {
-	
-	private static final transient Logger LOG = LoggerFactory.getLogger(Driver.class);
-	private FilterQuery filter = new FilterQuery();
-    private String[] keywordsArray = {"Hobbit","avenger"};
-    
-	public static void main(String[] args)
-	{
-		Driver dr = new Driver();
-		dr.filter.track(dr.keywordsArray);
-		LOG.info("Initializing...");
-		LOG.debug("Creating Listener...");
-		EnglishStatusListener listener = new EnglishStatusListener();
-		LOG.debug("Creating Stream...");
-	    TwitterStream twitterStream = new TwitterStreamFactory(ConfigBuilder.getConfig()).getInstance();
-	    twitterStream.addListener(listener);
-	    twitterStream.filter(dr.filter);
-	    
-	    
-	       
-	  
-	    LOG.info("Initialization Complete.");
-	   // twitterStream.sample();
-	}
 
+      LOG.info("Initialization Complete.");
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+  }
 }
